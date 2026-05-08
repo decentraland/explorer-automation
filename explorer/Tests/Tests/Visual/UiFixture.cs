ea632fac907dd047740a6a4d8838f0a384b57479
Pravus <pravusjif@gmail.com>
2026-05-08 20:05:56 +0200

    updated baselines + increased tolerance in tests + increased frames wait

--- explorer/Tests/Tests/Visual/UiFixture.cs
+++ explorer/Tests/Tests/Visual/UiFixture.cs
@@ -17,6 +17,6 @@ public class UiFixture
     public void Default()
     {
         Frame.WaitForStable();
-        Snapshot.AssertMatchesBaseline("default", tolerance: 0.5);
+        Snapshot.AssertMatchesBaseline("default", tolerance: 3);
     }
 }
