f76cc218b64966e0f22f9da5b95496dcc648600e
nearnshaw <earnshaw.nico@gmail.com>
2026-05-08 11:03:20 -0300

    textshape scene

---
+++ explorer/Tests/Tests/Visual/TextShapeFixture.cs
@@ -0,0 +1,22 @@
+namespace ExplorerAutomation.Tests.Tests.Visual;
+
+[TestFixture]
+[AllureNUnit]
+[AllureSuite("Visual")]
+[AllureSubSuite("text-shape")]
+[Category("Visual")]
+public class TextShapeFixture
+{
+    [OneTimeSetUp]
+    public void LoadScene()
+    {
+        VisualHost.Load("text-shape");
+    }
+
+    [Test]
+    public void Default()
+    {
+        Frame.WaitForStable();
+        Snapshot.AssertMatchesBaseline("default", tolerance: 0.5);
+    }
+}
