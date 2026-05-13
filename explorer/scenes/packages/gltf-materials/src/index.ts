45778de099b31065e74211055fe81a0c6bd17404
nearnshaw <earnshaw.nico@gmail.com>
2026-05-13 09:20:25 -0300

    gltf tests with Blender MCP

---
+++ explorer/scenes/packages/gltf-materials/src/index.ts
@@ -0,0 +1,19 @@
+import { engine, GltfContainer, Transform } from '@dcl/sdk/ecs'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
+import { setupVisualTest } from './visual-test-setup'
+
+export function main() {
+  const materialGrid = engine.addEntity()
+  Transform.create(materialGrid, {
+    position: Vector3.create(8, 8, 8),
+    rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+  })
+  GltfContainer.create(materialGrid, {
+    src: 'assets/Models/gltf_material_test_grid.glb',
+  })
+
+  setupVisualTest({
+    lookAtPos: Vector3.create(8, 8, 8),
+    cameraPos: Vector3.create(8, 8, -1.5),
+  })
+}
