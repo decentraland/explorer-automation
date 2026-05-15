9b08ae249ed9a7affa69aee9fea29b299222e5b6
Pravus <pravusjif@gmail.com>
2026-05-15 16:27:33 +0200

    decreased tolerance in tests + added timeout for loading screen await

--- explorer/Tests/Tests/Visual/GltfFixture.cs
+++ explorer/Tests/Tests/Visual/GltfFixture.cs
@@ -18,6 +18,6 @@ public class GltfFixture
     public void Default()
     {
         SceneReady.WaitUntilReady();
-        Snapshot.AssertMatchesBaseline("default", tolerance: 3);
+        Snapshot.AssertMatchesBaseline("default", tolerance: 1);
     }
 }
