7c17ccc25ee031d2caa06c9f2910dda236c18d00
Mikhail Agapov <mikhail.agapov@decentraland.org>
2026-09-10 11:58:30 +0300

    fix: retain WPR process exit status on Windows PowerShell

--- explorer/ci/test-native-wait-capture.ps1
+++ explorer/ci/test-native-wait-capture.ps1
@@ -22,15 +22,24 @@ $result = [ordered]@{
     buffer_mib = 64
 }
 function Invoke-Recorder([string[]]$Arguments, [string]$LogName, [int]$TimeoutSeconds = 30) {
-    $command = Start-Process $wpr -WindowStyle Hidden -PassThru -ArgumentList $Arguments `
-        -RedirectStandardOutput (Join-Path $OutputDirectory ($LogName + '.log')) `
-        -RedirectStandardError (Join-Path $OutputDirectory ($LogName + '-error.log'))
+    $command = New-Object Diagnostics.Process
+    $command.StartInfo.FileName = $wpr
+    $command.StartInfo.Arguments = $Arguments -join ' '
+    $command.StartInfo.UseShellExecute = $false
+    $command.StartInfo.CreateNoWindow = $true
+    $command.StartInfo.RedirectStandardOutput = $true
+    $command.StartInfo.RedirectStandardError = $true
     try {
+        if (-not $command.Start()) { throw "Could not start WPR $LogName" }
+        $stdout = $command.StandardOutput.ReadToEndAsync()
+        $stderr = $command.StandardError.ReadToEndAsync()
         if (-not $command.WaitForExit($TimeoutSeconds * 1000)) {
             $command.Kill()
             throw "WPR $LogName exceeded $TimeoutSeconds seconds"
         }
         $command.WaitForExit()
+        [IO.File]::WriteAllText((Join-Path $OutputDirectory ($LogName + '.log')), $stdout.GetAwaiter().GetResult())
+        [IO.File]::WriteAllText((Join-Path $OutputDirectory ($LogName + '-error.log')), $stderr.GetAwaiter().GetResult())
         if ($command.ExitCode -ne 0) { throw "WPR $LogName exit code $($command.ExitCode)" }
     } finally { $command.Dispose() }
 }
